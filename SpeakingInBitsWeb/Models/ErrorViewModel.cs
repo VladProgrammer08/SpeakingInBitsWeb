7c25c1cc439ca2cd424db1082287c218fddbe02e
VladProgrammer08 <bumbuv136@gmail.com>
2024-04-02 21:10:30 -0700

    Add project files.

---
+++ SpeakingInBitsWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpeakingInBitsWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
