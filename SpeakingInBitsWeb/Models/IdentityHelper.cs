d0878d987c1ab4e52e34ef2b18e62c1f18d252ed
VladProgrammer08 <bumbuv136@gmail.com>
2024-04-10 22:13:00 -0700

    Added default instructor account

--- SpeakingInBitsWeb/Models/IdentityHelper.cs
+++ SpeakingInBitsWeb/Models/IdentityHelper.cs
@@ -21,5 +21,23 @@ namespace SpeakingInBitsWeb.Models
                 }
             }
         }
+
+        public static async Task CreateDefaultUser(IServiceProvider provider, string role)
+        {
+            var userManager = provider.GetService<UserManager<IdentityUser>>();
+            // If no users are present, make the default user
+            int numUsers = (await userManager.GetUsersInRoleAsync(role)).Count();
+            if (numUsers == 0) // If no users are in the specified role
+            {
+                var defaultUser = new IdentityUser()
+                {
+                    Email = "instructor@speakinginbits.com",
+                    UserName = "Admin"
+                };
+                await userManager.CreateAsync(defaultUser, "Password01#");
+
+                await userManager.AddToRoleAsync(defaultUser, role);
+            }
+        }
     }
 }
